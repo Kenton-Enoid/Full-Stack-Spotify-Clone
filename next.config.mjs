b9ed3709ea8b5116d212e993eba3a179665b2283
Bonolo Gaosenkwe <bonolo.kamolane@gmail.com>
2024-09-17 16:25:12 +0200

    Added songs and images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "cjwpjqhzhxxrvbrnzzog.supabase.co"
+    ]
+  }
+};
 
 export default nextConfig;
