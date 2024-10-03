a2d1e0fc623f75600c6cb06e13c9cb7471bc1adb
Bonolo Gaosenkwe <bonolo.kamolane@gmail.com>
2024-10-03 12:03:23 +0200

    AccountContent file edited

--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-
-import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
-
-import Button from "@/components/Button"; 
+import React, { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
 import useSubscribeModal from "@/hooks/useSubscribeModal";
+import { useUser } from "@/hooks/useUser";
 import { postData } from "@/libs/helpers";
-import useUser from "@/hooks/useUser";
-
+import Button from "@/components/Button";
 
 const AccountContent = () => {
   const router = useRouter();
@@ -20,35 +17,31 @@ const AccountContent = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (!isLoading && !user) {
-      router.replace('/');
-    }
+    if (!isLoading && !user) router.replace("/");
   }, [isLoading, user, router]);
 
   const redirectToCustomerPortal = async () => {
     setLoading(true);
+
     try {
-      const { url, error } = await postData({
-        url: '/api/create-portal-link'
+      const { url } = await postData({
+        url: "/api/create-portal-link"
       });
       window.location.assign(url);
     } catch (error) {
-      if (error) {
-        toast.error((error as Error).message);
-      }
+      console.error(error);
+      toast.error((error as Error).message);
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
-  }
+  };
 
   return (
     <div className="mb-7 px-6">
       {!subscription && (
         <div className="flex flex-col gap-y-4">
-          <p>No active plan</p>
-          <Button
-            onClick={subscribeModal.onOpen}
-            className="w-[300px]"
-          >
+          <p>No Active Plan.</p>
+          <Button className="w-[300px]" onClick={subscribeModal.onOpen}>
             Subscribe
           </Button>
         </div>
@@ -56,19 +49,20 @@ const AccountContent = () => {
       {subscription && (
         <div className="flex flex-col gap-y-4">
           <p>
-            You are currently on the <b>{subscription?.prices?.products?.name}</b> plan
+            You are currently on the{" "}
+            <b>{subscription?.prices?.products?.name}</b> plan.
           </p>
           <Button
+            className="w-[300px]"
             disabled={loading || isLoading}
             onClick={redirectToCustomerPortal}
-            className="w-[300px]"
           >
-            Open customer portal
+            Open Customer Portal
           </Button>
         </div>
       )}
     </div>
   );
-}
+};
 
 export default AccountContent;
